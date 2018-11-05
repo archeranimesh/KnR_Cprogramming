83fd89ec6065cdc9e6a115863f2c23db2445b96f
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2018-11-05 23:12:45 +0530

    Hello World to the C programming lanugae

---
+++ 01_chapter/solvedExamples/code/src/hello.c
@@ -0,0 +1,11 @@
+/*include information about the standard library. */
+#include<stdio.h>
+
+/* Program execution always begins with a special function name main() */
+int main()
+{
+    /* main calls library funtion printf, to print
+     * this sequence of character, \n represent new line.  */
+    printf("hello, world\n");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# The C Programming Language, 2nd Edition
+
+Solved examples, unsolved excercise from the book written by Brain W. Kernighan and Dennis M. Ritchie.
